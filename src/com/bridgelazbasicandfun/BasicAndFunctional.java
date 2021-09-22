61e42a4be3f3d0aacab1b265f317405da2d1b7ee
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-22 13:13:27 +0530

    Added:checking power of two program

--- src/com/bridgelazbasicandfun/BasicAndFunctional.java
+++ src/com/bridgelazbasicandfun/BasicAndFunctional.java
@@ -5,23 +5,22 @@ import java.util.Scanner;
 public class BasicAndFunctional {
 
 	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Enter Year Here : ");
-	    int Year = sc.nextInt();
-	    //Ensure it is a 4 digit number
-	    if(Year > 999 && Year < 9999)
-	        checkLeapYear(Year);
-	    else
-	        System.out.println("Enter 4 digit Year");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int Num = sc.nextInt();
+        powerOfNumber(Num);
+    }
+    public static void powerOfNumber(int input){
+        int powerOfTwo = 1;
+        if(input > 0 && input < 31) {
+            for (int i = 1; i <= input; i++) {
+                powerOfTwo = powerOfTwo * 2;
+                System.out.println(2 + "to the Power " +i+ " : "+powerOfTwo);
+            }
+        }
+        else{
+            System.out.println("your overflows int datatype limit");
+        }
 
-	}
-	    public static void checkLeapYear(int Year){
-	        // Checking condition of leap year
-	        if ((Year % 4 == 0) && (Year % 100 != 0) || (Year %400 == 0 ) )
-	            System.out.println(+Year+ " is leap year ");
-	        else
-	            System.out.println(+Year+ " is not leap year");
-
-	}
-
+    }
 }
