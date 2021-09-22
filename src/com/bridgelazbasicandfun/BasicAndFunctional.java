9d4fb6815f961fad9607e507095c0d5d41495f91
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-22 14:35:34 +0530

    Added:Euclidean distance

--- src/com/bridgelazbasicandfun/BasicAndFunctional.java
+++ src/com/bridgelazbasicandfun/BasicAndFunctional.java
@@ -3,42 +3,30 @@ package com.bridgelazbasicandfun;
 import java.util.Scanner;
 
 public class BasicAndFunctional {
-	static void sunOfThree(int n[]) {
-        int count=0;
-        for (int i = 0; i < n.length-2; i++)
-        {
-            for (int j = i+1; j < n.length-1; j++)
-            {
-                for (int k =j+1; k < n.length; k++)
-                {
-                    if(n[i]+n[j]+n[k]==0)
-                    {
-                        System.out.println(n[i]+" + "+n[j]+" + "+n[k]+" = " +0);
-
-                        count++;
-                    }
-                }
-            }
-
-        }
-        System.out.println("The number of triple count ="+count);
-
+	public static void main(String[] args) {
+        /* that takes two integer command-line
+         * arguments x and y
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the integer for x");
+        int x = sc.nextInt();
+        System.out.println("Enter the integer for x");
+        int y = sc.nextInt();
+        calDistance(x, y);
+        sc.close();
     }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Array length");
-        int len=sc.nextInt();
-        int n[]=new int[len];
 
-        System.out.println("Enter the "+len+" Array ");
-        for (int i = 0; i < n.length; i++)
-        {
-            n[i]=sc.nextInt();
-        }
-        sunOfThree(n);
-
-        }
+    private static void calDistance(int x, int y) {
+        /* prints the Euclidean distance from the point (x, y) to
+         * the origin (0, 0). The formulae to calculate
+         * distance = sqrt(x*x + y*y). Use Math.power function
+         */
+        double res = (Math.sqrt((Math.pow(x, 2)) + (Math.pow(y, 2))));
+        System.out.println("The Euclidean distance is: " + res);
     }
+
+        }
+    
     
     
 
