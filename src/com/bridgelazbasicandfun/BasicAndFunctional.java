853eec3bc4271e60f17812a904b085ab7b161c57
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-22 14:25:26 +0530

    Added:2-D array

--- src/com/bridgelazbasicandfun/BasicAndFunctional.java
+++ src/com/bridgelazbasicandfun/BasicAndFunctional.java
@@ -3,29 +3,41 @@ package com.bridgelazbasicandfun;
 import java.util.Scanner;
 
 public class BasicAndFunctional {
-	public static void main(String[] args)
-    {
-        int a, b, c;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter first number:");
-        a = input.nextInt();
-        System.out.print("Enter the second number:");
-        b = input.nextInt();
-        System.out.print("Enter the third number:");
-        c = input.nextInt();
-        if(a > b && a >c)
-        {
-            System.out.println("Largest number is:"+a);
+	static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        /*
+        *row and cols size
+        * using Scanner object class
+         */
+        System.out.println("Enter The Row Size : ");
+        int M = sc.nextInt();
+        System.out.println("Enter The Column Size : ");
+        int N = sc.nextInt();
+        getArray(M,N);
+    }
+
+
+    static void getArray(int X, int Y){
+        /*
+        *enter input in array
+        * and store it
+         */
+        int [] [] arr = new int[X][Y];
+        for (int i=0; i<X;i++){
+            for (int j=0; j<Y;j++){
+                System.out.print("Enter value : "+i+" "+j+" : ");      // for printing every position
+                arr[i][j]= sc.nextInt();
+            }
         }
-        else if(a > c)
-        {
-            System.out.println("Largest number is:"+b);
-        }
-        else
-        {
-            System.out.println("Largest number is:"+c);
+        sc.close();
+        for (int i=0; i<X;i++) {
+            for (int j = 0; j < Y; j++) {
+                System.out.print(arr[i][j] + " "); //display array in 2D
+            }
+            System.out.println(" ");
         }
     }
     }
+    
 
 
